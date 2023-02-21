51fc1646500f7247d6052f2bbb96037453007d01
Vito Korengkeng <s2200432@student.unklab.ac.id>
2023-02-21 09:56:05 +0800

    Update file

--- Exxercise#3/index.js
+++ Exxercise#3/index.js
@@ -1,4 +1,36 @@
-const http        = require("http");
+//Express
+const express = require('express');
+const dataMember = require('./members.js');
+const dataUsers = require('./users.js');
+const moment = require('moment');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('This Is the Home Page');
+});
+
+app.get('/about', (req, res) => {
+  res.json({
+    status: 'success',
+    message: 'response success',
+    description: 'Exercise #03',
+    date: moment().format(),
+    data: dataMember,
+  });
+});
+
+app.get('/users', (req, res) => {
+  res.json(dataUsers);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
+
+/*const http        = require("http");
 const dataMember  = require("./members.js");
 const dataUsers   = require("./users.js");
 const moment      = require("moment");
@@ -35,3 +67,5 @@ const port = 3000;
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+*/
